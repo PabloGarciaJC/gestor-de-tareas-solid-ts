6f479a3f2dbb31bd69cb64c17bcc13f4bc9520d4
PabloGarciaJC <pablogarciajc@hotmail.com>
2025-11-09 13:25:03 +0100

    se elimina archivos innecesarios

--- src/SingleResponsibilityPrinciple/Task.tsx
+++
@@ -1,6 +0,0 @@
-// SRP: Este archivo solo define qué es una tarea.
-export interface Task {
-  id: number;
-  title: string;
-  completed: boolean;
-}
--- src/SingleResponsibilityPrinciple/TaskService.tsx
+++
@@ -1,9 +0,0 @@
-// import { Task } from "./Task";
-
-// // SRP: Esta clase solo tiene responsabilidad de ACCESO A DATOS (API).
-// export class TaskService {
-//   async fetchTasks(): Promise<Task[]> {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
-//     return response.json();
-//   }
-// }
